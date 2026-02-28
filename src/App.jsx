277315b9f8c7c10b8f5ff9425be51c20b2e9a91f
JariBesonen <149715363+JariBesonen@users.noreply.github.com>
2026-02-28 14:24:47 -0700

    Add navbar for destop

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react"
-import Home from "./Pages/Home/Home"
+import Home from "./Pages/Home/Home";
+import Navbar from "./Components/Navbar/Navbar";
 function App() {
   
 
   return (
     <>
+    <Navbar />
       <Home />
     </>
   )
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import "./Navbar.css";
+function Navbar() {
+  return (
+    <nav>
+      <h1>JB</h1>
+      <ul>
+        <li>Projects</li>
+        <li>Toolkit</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
