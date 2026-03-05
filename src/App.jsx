34485ecdf36dad8060e210ee070b9af2adc4139a
JariBesonen <149715363+JariBesonen@users.noreply.github.com>
2026-03-05 12:29:34 -0700

    Remove hamburger nav files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import Home from "./Pages/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
+
 function App() {
   
 
   return (
     <>
+   
     <Navbar />
       <Home />
     </>
